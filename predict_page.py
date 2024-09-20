acf275ff2805a42012c4bfb93ce5b2f3b5ff0ae6
Agullina Shllaku <agullina.s@gmail.com>
2024-09-20 13:21:23 +0200

    Updated the mapping

--- predict_page.py
+++ predict_page.py
@@ -11,7 +11,7 @@ def show_predict_page():
     hypertension = st.selectbox("Do you have hypertension?", options=["Yes", "No"])
     heart_disease = st.selectbox("Do you have heart disease?", options=["Yes", "No"])
     ever_married = st.selectbox("Have you ever been married?", options=["Yes", "No"])
-    work_type = st.selectbox("Work Type", options=["Private", "Self-employed", "Govt_job", "Children", "Never_worked"])
+    work_type = st.selectbox("Work Type", options=["Private", "Self-employed", "Governemnt Job", "Stay-at-home parent", "Never worked"])
     residence_type = st.selectbox("Residence Type", options=["Urban", "Rural"])
     avg_glucose_level = st.number_input("Average Glucose Level", min_value=0.0, step=0.1)
     bmi = st.number_input("BMI", min_value=0.0, step=0.1)
@@ -20,6 +20,14 @@ def show_predict_page():
     # Map user input to numerical values
     hypertension = 1 if hypertension == "Yes" else 0
     heart_disease = 1 if heart_disease == "Yes" else 0
+    if work_type == "Governemnt Job":
+        work_type = "Govt_job"
+    elif work_type == "Stay-at-home parent":
+        work_type = "Children"
+    elif work_type == "Never worked":
+        work_type = "Never_worked"
+    else:
+        work_type
 
     # Create a DataFrame with the mapped user input
     user_input = pd.DataFrame({
@@ -38,6 +46,8 @@ def show_predict_page():
     # Make prediction and get probabilities using the pipeline
     if st.button('Predict Stroke Probability'):
         probability = pipe.predict_proba(user_input)[0][1]  # Probability of class 1 (stroke)
-        st.write(f"Probability of Stroke: {probability*100:.2f}")
-
+        st.empty()
+        st.write(f"# Probability of Stroke: {probability*100:.2f}")
+        st.empty()
+        
 show_predict_page()
